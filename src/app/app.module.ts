17e41cfe74cc7504ca41149c2b991d7a9ad1fe2c
Moisés Estevão <moisesestevao2004@hotmail.com>
2022-04-22 13:02:54 -0300

    cleaning mess up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
 import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
 
 
 @NgModule({
@@ -25,7 +22,6 @@ import { ExamplesModule } from './examples/examples.module';
     FormsModule,
     RouterModule,
     ComponentsModule,
-    ExamplesModule,
     AppRoutingModule,
   ],
   providers: [],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,16 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 import { RouterModule } from '@angular/router';
-
-import { BasicelementsComponent } from './basicelements/basicelements.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { TypographyComponent } from './typography/typography.component';
-import { NucleoiconsComponent } from './nucleoicons/nucleoicons.component';
 import { ComponentsComponent } from './components.component';
-import { NotificationComponent } from './notification/notification.component';
-import { NgbdModalComponent } from './modal/modal.component';
-import { NgbdModalContent } from './modal/modal.component';
-
 @NgModule({
     imports: [
         CommonModule,
@@ -26,15 +17,7 @@ import { NgbdModalContent } from './modal/modal.component';
     ],
     declarations: [
         ComponentsComponent,
-        BasicelementsComponent,
-        NavigationComponent,
-        TypographyComponent,
-        NucleoiconsComponent,
-        NotificationComponent,
-        NgbdModalComponent,
-        NgbdModalContent
     ],
-    entryComponents: [NgbdModalContent],
     exports:[ ComponentsComponent ]
 })
 export class ComponentsModule { }
